cbf397a1eae10ee2d51343e0bc1a427a171e8c72
MarShin <martinshin95@gmail.com>
2022-09-10 18:21:04 +0800

    ..

--- examples/rllib/self_play_train.py
+++ examples/rllib/self_play_train.py
@@ -27,7 +27,6 @@ from meltingpot.python import substrate
 
 
 def main():
-<<<<<<< HEAD
   # We need the following 2 pieces to run the training:
   # 1. The agent algorithm to use.
   agent_algorithm = "PPO"
@@ -38,33 +37,11 @@ def main():
   # 1. Gets default training configuration and specifies the POMgame to load.
   config = copy.deepcopy(
       get_trainer_class(agent_algorithm).get_default_config())
-=======
-    # We need the following 3 pieces to run the training:
-    # 1. The agent algorithm to use.
-    agent_algorithm = "PPO"
-    # 2. The name of the MeltingPot substrate, coming
-    # from substrate.AVAILABLE_SUBSTRATES.
-    substrate_name = "allelopathic_harvest"
-    # 3. The number of CPUs to be used to run the training.
-    num_cpus = 1
-
-    # function that outputs the environment you wish to register.
-    def env_creator(env_config):
-        env = substrate.build(config_dict.ConfigDict(env_config))
-        env = multiagent_wrapper.MeltingPotEnv(env)
-        return env
-
-    # 1. Gets default training configuration and specifies the POMgame to load.
-    config = copy.deepcopy(
-        get_trainer_class(agent_algorithm)._default_config
-    )  # pylint: disable=protected-access
->>>>>>> f15c8bc (temp fix of agent_id)
 
     # 2. Set environment config. This will be passed to
     # the env_creator function via the register env lambda below.
     config["env_config"] = substrate.get_config(substrate_name)
 
-<<<<<<< HEAD
   # 3. Register env
   register_env("meltingpot", utils.env_creator)
   config["env"] = "meltingpot"
@@ -110,41 +87,6 @@ def main():
   # maximum one done=True in the last step of the trajectory.
   # If no_done_at_end = True, environment is not resetted
   # when dones[__all__]= True.
-=======
-    # 3. Register env
-    register_env("meltingpot", env_creator)
-
-    # 4. Extract space dimensions
-    test_env = env_creator(config["env_config"])
-    obs_space = test_env.single_player_observation_space()
-    act_space = test_env.single_player_action_space()
-
-    # 5. Configuration for multiagent setup with policy sharing:
-    config["multiagent"] = {
-        "policies": {
-            # the first tuple value is None -> uses default policy
-            "av": (None, obs_space, act_space, {}),
-        },
-        "policy_mapping_fn": lambda agent_id, **kwargs: "av",
-    }
-
-    # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
-    config["num_gpus"] = int(os.environ.get("RLLIB_NUM_GPUS", "0"))
-    config["log_level"] = "DEBUG"
-    config["num_workers"] = 1
-    # Fragment length, collected at once from each worker and for each agent!
-    config["rollout_fragment_length"] = 30
-    # Training batch size -> Fragments are concatenated up to this point.
-    config["train_batch_size"] = 200
-    # After n steps, force reset simulation
-    config["horizon"] = 2000
-    # Default: False
-    config["no_done_at_end"] = False
-    # Info: If False, each agents trajectory is expected to have
-    # maximum one done=True in the last step of the trajectory.
-    # If no_done_at_end = True, environment is not resetted
-    # when dones[__all__]= True.
->>>>>>> f15c8bc (temp fix of agent_id)
 
     # Setup for the neural network.
     config["model"] = {}
@@ -166,7 +108,6 @@ def main():
     config["model"]["lstm_use_prev_reward"] = False
     config["model"]["lstm_cell_size"] = 256
 
-<<<<<<< HEAD
   # 6. Initialize ray, train and save
   ray.init()
 
@@ -177,17 +118,6 @@ def main():
       metric="episode_reward_mean",
       mode="max",
       checkpoint_at_end=True)
-=======
-    # 6. Initialize ray and trainer object
-    ray.init(
-        num_cpus=num_cpus + 1,
-        # include_dashboard=False
-    )
-    trainer = get_trainer_class(agent_algorithm)(env="meltingpot", config=config)
-
-    # 7. Train once
-    trainer.train()
->>>>>>> f15c8bc (temp fix of agent_id)
 
 
 if __name__ == "__main__":
